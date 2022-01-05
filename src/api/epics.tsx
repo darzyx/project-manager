1701e29301823273575cb435af9c0272595dd677
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-05 16:57:21 -0500

    Add more fields and descriptions

--- src/api/epics.tsx
+++ src/api/epics.tsx
@@ -4,10 +4,12 @@ const epics = {
         name: "Example Epic Name",
         description: "This is an example epic description.",
         date_created: "January 1st, 2022",
-        points: 1,
+        points: 1, // Depends on associated stories
+        state: "active",
         date_due: "February 1st, 2022",
-        tags: ["epic-example-tag", "another-example-tag"],
-        attachments: []
+        labels: ["epic-example-tag", "another-example-tag"],
+        attachments: [],
+        stories: ["zero"], // Depends on associated stories
     }
 }
 
--- src/api/stories.tsx
+++ src/api/stories.tsx
@@ -5,13 +5,15 @@ const stories = {
         description: "This is an example story description.",
         date_created: "January 1st, 2022",
         points: 1,
+        state: "backlogged", // Can depend on epic (if epic is active, everything is at least "todo")
+        type: "feature", // One of feature, bug, chore
         assignee: "Dario Sanchez",
         requester: "Example Requester",
         priority: "high",
         epic_name: "Example Epic Name",
         epic_uuid: "zero",
-        tags: ["example-story-tag", "another", "one-more"],
-        attachments: []
+        labels: ["example-story-tag", "another", "one-more"],
+        attachments: [],
     }
 }
 
