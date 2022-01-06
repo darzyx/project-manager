a56a8067c3bd04c8ec8457f05993600a5fcdf239
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-05 22:12:06 -0500

    Format code and reduce base font size to 12px

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,52 @@
-import { List, Label, Segment, Divider, Icon } from "semantic-ui-react";
+import { List, Label, Segment, Grid, Icon } from "semantic-ui-react";
 
 import stories from "api/stories";
 
-console.log({ stories });
+const Column = () => (
+  <Segment inverted>
+    <List divided relaxed inverted>
+      {Object.values(stories).map((story, index) => (
+        <List.Item key={index}>
+          <Icon name="triangle right" size="small" />
+          <List.Content>
+            <List.Header>{story.name}</List.Header>
+            <List.Description>{story.description}</List.Description>
+            <div style={{ margin: "5px 0 0 0" }}>
+              {story.labels.map((label, index) => (
+                <Label key={index} basic color="blue" size="mini">
+                  {label}
+                </Label>
+              ))}
+            </div>
+          </List.Content>
+        </List.Item>
+      ))}
+    </List>
+  </Segment>
+);
 
 const App = () => {
   return (
-    <div>
-      <Segment inverted>
-        <List divided relaxed inverted>
-          {Object.values(stories).map((story, index) => (
-            <List.Item key={index}>
-              <Icon name="triangle right" size="small" />
-              <List.Content>
-                <List.Header>{story.name}</List.Header>
-                <List.Description>{story.description}</List.Description>
-                <span>
-                  {story.labels.map((label, index) => (
-                    <Label key={index} basic color="blue" size="mini">
-                      {label}
-                    </Label>
-                  ))}
-                </span>
-              </List.Content>
-            </List.Item>
-          ))}
-        </List>
-      </Segment>
+    <div className="App">
+      <Grid columns={5} divided>
+        <Grid.Row>
+          <Grid.Column>
+            <Column />
+          </Grid.Column>
+          <Grid.Column>
+            <Column />
+          </Grid.Column>
+          <Grid.Column>
+            <Column />
+          </Grid.Column>
+          <Grid.Column>
+            <Column />
+          </Grid.Column>
+          <Grid.Column>
+            <Column />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 };
--- src/api/stories.tsx
+++ src/api/stories.tsx
@@ -1,7 +1,7 @@
 const stories = {
   zero: {
     uuid: "zero",
-    name: "Example Story Name 1",
+    name: "Example Story Name 0",
     description: "This is an example story description.",
     date_created: "January 1st, 2022",
     points: 1,
@@ -17,6 +17,22 @@ const stories = {
   },
   one: {
     uuid: "one",
+    name: "Example Story Name 1",
+    description: "This is an example story description.",
+    date_created: "January 8st, 2022",
+    points: 1,
+    state: "backlogged", // Can depend on epic (if epic is active, everything is at least "todo")
+    type: "feature", // One of feature, bug, chore
+    assignee: "Dario Sanchez",
+    requester: "Example Requester",
+    priority: "high",
+    epic_name: "Example Epic Name",
+    epic_uuid: "zero",
+    labels: ["example-story-tag", "another", "one-more"],
+    attachments: [],
+  },
+  two: {
+    uuid: "two",
     name: "Example Story Name 2",
     description: "This is an example story description.",
     date_created: "January 8st, 2022",
