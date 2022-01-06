69bae0dcf610e692724dd2913fce19369fe40c7b
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-05 19:31:36 -0500

    Add example component and add format script

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,17 @@ import { List, Segment } from "semantic-ui-react";
 
 const App = () => {
   return (
-    <div>
-      <p>Hello, world</p>
+    <div className="App">
+      <Segment inverted>
+        <List divided inverted>
+          <List.Item>
+            <List.Content>
+              <List.Header>Snickerdoodle</List.Header>
+              An excellent companion
+            </List.Content>
+          </List.Item>
+        </List>
+      </Segment>
     </div>
   );
 };
