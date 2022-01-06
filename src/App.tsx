72080b880dec120e111509acbcee06346437c0a1
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-05 19:18:31 -0500

    Remove unused elements and set colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import './App.css';
+import { List, Segment } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Project Manager</h1>
-        <p>A project manager tool that does exactly what I want.</p>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  return (<div>
+    <p>Hello, world</p>
+  </div>)
 }
 
 export default App;
