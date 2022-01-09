777670b01f48b20f3ad809952d24838e28f7b76c
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-09 01:01:46 -0500

    Add sticky header to dashboard page

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,5 @@
-import { Grid, Header, Divider } from "semantic-ui-react";
+import { Component, createRef, RefObject } from "react";
+import { Grid, Header, Divider, Ref, Sticky, Segment } from "semantic-ui-react";
 
 import stories from "api/stories";
 
@@ -6,48 +7,79 @@ import AppMenu from "./AppMenu";
 import DashboardMenu from "components/dashboard/DashboardMenu";
 import Story from "components/stories/Story";
 
-const StoriesList = () => (
-  <>
-    <Header as="h3" inverted textAlign="center">
-      Column Header
-    </Header>
-    {Object.values(stories).map((story, index) => (
-      <Story key={index} story={story} />
-    ))}
-  </>
+const StickySegment = ({
+  name,
+  context,
+}: {
+  name: string;
+  context: RefObject<HTMLDivElement>;
+}) => (
+  <Sticky context={context}>
+    <Segment
+      inverted
+      textAlign="center"
+      style={{ borderRadius: "0", borderBottom: "1px solid black" }}
+    >
+      {name}
+    </Segment>
+  </Sticky>
 );
 
-const App = () => {
-  return (
-    <div>
-      <AppMenu />
-      <div className="App-content">
-        <Header as="h2" inverted>
-          Dashboard
-          <Header.Subheader>March 6th, 2022</Header.Subheader>
-        </Header>
-        <Divider hidden />
-        <DashboardMenu />
-        <Divider hidden />
-        <Grid columns={4} divided stackable>
-          <Grid.Row>
-            <Grid.Column>
-              <StoriesList />
-            </Grid.Column>
-            <Grid.Column>
-              <StoriesList />
-            </Grid.Column>
-            <Grid.Column>
-              <StoriesList />
-            </Grid.Column>
-            <Grid.Column>
-              <StoriesList />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </div>
-    </div>
-  );
-};
+export default class App extends Component {
+  contextRef = createRef<HTMLDivElement>();
 
-export default App;
+  render() {
+    return (
+      <div>
+        <AppMenu />
+        <div className="App-content">
+          <Header as="h2" inverted>
+            Dashboard
+            <Header.Subheader>March 6th, 2022</Header.Subheader>
+          </Header>
+          <Divider hidden />
+          <DashboardMenu />
+          <Divider hidden />
+          <Ref innerRef={this.contextRef}>
+            <div>
+              <Grid columns={5} divided stackable>
+                <Grid.Row>
+                  <Grid.Column>
+                    <StickySegment name="Scheduled" context={this.contextRef} />
+                    {Object.values(stories).map((story, index) => (
+                      <Story key={index} story={story} />
+                    ))}
+                  </Grid.Column>
+                  <Grid.Column>
+                    <StickySegment name="Started" context={this.contextRef} />
+                    {Object.values(stories).map((story, index) => (
+                      <Story key={index} story={story} />
+                    ))}
+                  </Grid.Column>
+                  <Grid.Column>
+                    <StickySegment name="Reviewing" context={this.contextRef} />
+                    {Object.values(stories).map((story, index) => (
+                      <Story key={index} story={story} />
+                    ))}
+                  </Grid.Column>
+                  <Grid.Column>
+                    <StickySegment name="Deployed" context={this.contextRef} />
+                    {Object.values(stories).map((story, index) => (
+                      <Story key={index} story={story} />
+                    ))}
+                  </Grid.Column>
+                  <Grid.Column>
+                    <StickySegment name="Confirmed" context={this.contextRef} />
+                    {Object.values(stories).map((story, index) => (
+                      <Story key={index} story={story} />
+                    ))}
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </div>
+          </Ref>
+        </div>
+      </div>
+    );
+  }
+}
