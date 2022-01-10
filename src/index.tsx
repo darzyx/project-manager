7e21e0eea1fb070c1c64ba13070c5a7dbf3ebedf
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-10 18:33:04 -0500

    Move styles to separate directory and add colors obj

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./semantic-ui-css/semantic.css";
-import "./index.css";
+import "./styles/semantic-ui-css/semantic.css";
+import "./styles/index.css";
 import App from "./components/app/App";
 import reportWebVitals from "./reportWebVitals";
 
---
+++ src/styles/colors.tsx
@@ -0,0 +1,9 @@
+const colors: { [name: string]: string } = {
+  sunset_orange: "#F85149",
+  anzac: "#E3B341",
+  forest_green: "#238636",
+  malibu: "#58a6ff",
+  regent_gray: "#8b949e",
+};
+
+export default colors;
