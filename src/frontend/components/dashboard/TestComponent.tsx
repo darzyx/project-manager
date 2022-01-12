76be6d8d2d9cc8ae9f2688933b699e5c17c5f848
Jose Dario Sanchez <jose.sanchez@hireanesquire.com>
2022-01-11 21:43:51 -0500

    Rename test components

--- src/frontend/components/dashboard/TestComponent.tsx
+++ src/frontend/components/dashboard/TestComponent.tsx
@@ -1,53 +1,6 @@
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const TaskContainer = styled.div`
-  border: 1px solid green;
-  padding: 10px;
-`;
-
-const ColumnContainer = styled.div`
-  border: 1px solid red;
-  padding: 10px;
-`;
-
-const TaskList = styled.div`
-  border: 1px solid blue;
-  padding: 10px;
-`;
-
-type TaskType = { id: string; val: string };
-
-const Task = ({ task, index }: { task: TaskType; index: number }) => (
-  <Draggable draggableId={task.id} index={index}>
-    {(provided) => (
-      <TaskContainer
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-        ref={provided.innerRef}
-      >
-        {task.val}
-      </TaskContainer>
-    )}
-  </Draggable>
-);
-
-const Column = ({ column }: { column: { id: string; tasks: TaskType[] } }) => (
-  <ColumnContainer>
-    <h1>{column.id}</h1>
-    <Droppable droppableId={column.id}>
-      {(provided) => (
-        <TaskList ref={provided.innerRef} {...provided.droppableProps}>
-          {column.tasks.map((task, index) => (
-            <Task key={index} task={task} index={index} />
-          ))}
-          {provided.placeholder}
-        </TaskList>
-      )}
-    </Droppable>
-  </ColumnContainer>
-);
-
 const testData = [
   {
     id: "potato",
@@ -63,16 +16,88 @@ const testData = [
       { id: "d", val: "do another dumbo" },
     ],
   },
+  {
+    id: "totato",
+    tasks: [
+      { id: "e", val: "do a dumb thing" },
+      { id: "f", val: "do another dumbo" },
+    ],
+  },
+  {
+    id: "popapo",
+    tasks: [
+      { id: "g", val: "do a dumb thing" },
+      { id: "h", val: "do another dumbo" },
+    ],
+  },
 ];
 
-const TestComponent = () => (
-  <div>
-    <DragDropContext onDragEnd={(val) => console.log("Drag Ended", { val })}>
-      {testData.map((column, index) => (
-        <Column key={index} column={column} />
-      ))}
-    </DragDropContext>
-  </div>
+const StoryContainer = styled.div`
+  border: 1px solid green;
+  margin: 10px;
+  padding: 10px;
+`;
+type StoryPropsType = { task: { id: string; val: string }; index: number };
+const Story = ({ task, index }: StoryPropsType) => (
+  <Draggable draggableId={task.id} index={index}>
+    {(provided) => (
+      <StoryContainer
+        {...provided.draggableProps}
+        {...provided.dragHandleProps}
+        ref={provided.innerRef}
+      >
+        {task.val}
+      </StoryContainer>
+    )}
+  </Draggable>
 );
 
-export default TestComponent;
+const StoryListHeader = styled.h2``;
+const StoryList = styled.div`
+  border: 1px solid blue;
+  margin: 10px;
+  padding: 10px;
+`;
+const StoryColumnContainer = styled.div`
+  border: 1px solid red;
+  margin: 10px;
+  padding: 10px;
+`;
+type StoryColumnPropsType = {
+  column: { id: string; tasks: { id: string; val: string }[] };
+  index: number;
+};
+const StoryColumn = ({ column, index }: StoryColumnPropsType) => (
+  <StoryColumnContainer style={{ ...(index !== 0 && { marginLeft: "10px" }) }}>
+    <StoryListHeader>Column Header</StoryListHeader>
+    <Droppable droppableId={column.id}>
+      {(provided) => (
+        <StoryList ref={provided.innerRef} {...provided.droppableProps}>
+          {column.tasks.map((task, index) => (
+            <Story key={index} task={task} index={index} />
+          ))}
+          {provided.placeholder}
+        </StoryList>
+      )}
+    </Droppable>
+  </StoryColumnContainer>
+);
+
+const StoryColumnGroupContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  padding: 10px;
+  margin: 10px;
+  border: 1px solid skyblue;
+`;
+const StoryColumnGroup = () => (
+  <DragDropContext onDragEnd={(val) => console.log("Drag Ended", { val })}>
+    <StoryColumnGroupContainer>
+      {testData.map((column, index) => (
+        <StoryColumn key={index} index={index} column={column} />
+      ))}
+    </StoryColumnGroupContainer>
+  </DragDropContext>
+);
+
+export default StoryColumnGroup;
